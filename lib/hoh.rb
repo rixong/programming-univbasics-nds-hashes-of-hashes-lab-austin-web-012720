9da2c4eda1770695cccb95e4d73fc322769f3549
Rick Glascock <rixong@gmail.com>
2020-01-11 15:06:37 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -36,7 +36,13 @@ def naming_system
     sub_category: {
       label: "Phylum", 
       sub_category: {
-        label: "Class"
+        label: "Class",
+        sub_category: {
+          label: "Order",
+          sub_category: {
+            
+            }
+          }
         }
       }
     }
