ac4ea68ff05989dadc7a518152dcc9b82bb9bb09
Rick Glascock <rixong@gmail.com>
2020-01-11 15:08:35 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -40,7 +40,14 @@ def naming_system
         sub_category: {
           label: "Order",
           sub_category: {
-            
+            label: 'Family',
+            sub_category: {
+              label: 'Genus',
+              sub_category: {
+                label: Species,
+                sub_category: nil
+                }
+              }
             }
           }
         }
